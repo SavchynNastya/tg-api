0a210d6d0930e5dbc3e95d92289054a1b6634de5
Anastasiia Savchyn <sa@empat.tech>
2023-10-29 22:07:48 +0200

    migrations dropped

--- tg_api/settings.py
+++ tg_api/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'user_settings',
 ]
 
-AUTH_USER_MODEL = 'users.CustomUser'
+# AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
